6b50fcc59d35186a0e1163c452492eee17fc4f6d
vietnb25 <nhunnhun2503@gmail.com>
2025-07-18 15:08:47 +0700

    layout
    header
    herosection
    productinfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
-
+import Image from 'next/image';
+import { Row, Col } from 'react-bootstrap';
+import HeroSection from './component/herosection';
+import ProductInfo from './component/ProducInfo';
+import Features from './component/features';
+import ProductFeatures from './component/ProductFeatures';
 export default function Home() {
   return (
-    <div >
-      <h1 >Hello world</h1>
+    <div>
+      <HeroSection />
+
+      <ProductInfo />
+      <ProductFeatures />
     </div>
 
   );
